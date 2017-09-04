196fd43b15bae94b932d3524448c5e5b7c39cec0
Karan Sagoo <ksagoo.ee@gmail.com>
2017-09-03 17:24:10 -0700

    Create contact new page using state control (setState)

---
+++ contacts/src/CreateContact.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class CreateContact extends Component{
+  render(){
+    return(
+      <div>
+        Create Contact!
+      </div>
+    )
+  }
+
+}
+
+export default CreateContact
